97fd967b0ea3641a28174a0b5a0fbce47e512e26
soutahasimoto <hasimoto.s511@gmail.com>
2024-05-14 12:18:33 +0900

    3.4塗りつぶした楕円を描く

--- 初級java2.5/src/start1.java
+++ 初級java2.5/src/start1.java
@@ -4,7 +4,9 @@ public class start1 {
 	public static void main(String[] args) {
 		
        System.out.println("プログラムを実行します");
-       MyFrame frame1=new RectTest();
+       MyFrame frame1=new OvalTest();
+       
+       
        
        
 	}
