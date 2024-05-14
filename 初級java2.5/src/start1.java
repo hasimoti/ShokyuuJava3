1ab90ce52726f991cac16c27642d3f7b3d4c14f7
soutahasimoto <hasimoto.s511@gmail.com>
2024-05-14 10:45:57 +0900

    3.1塗りつぶした四角形を描く

--- 初級java2.5/src/start1.java
+++ 初級java2.5/src/start1.java
@@ -2,22 +2,9 @@
 public class start1 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-       /*MyFrame frame1=new MyFrame();
-        frame1.setLocation(200,100);
-        frame1.setSize(150,150);
-        */
-       MyFrame frame2 = new RectFrame();
-       frame2.setLocation(350,200);
-       
-       MyFrame frame3 = new RectFrame();
-       frame3.setLocation(50,300);
-       
-       int x=2,y=3;
-       System.out.println(x*y);
-       
-       MyFrame frame4 = new RectFrame();
-       frame4.setColor(255,0,0);
+		
+       System.out.println("プログラムを実行します");
+       MyFrame frame1=new RectTest();
        
        
 	}
