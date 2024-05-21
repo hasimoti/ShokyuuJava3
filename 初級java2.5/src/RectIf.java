c928a559e376c7835f2b94c71cda5feb186edf2e
soutahasimoto <hasimoto.s511@gmail.com>
2024-05-21 11:48:17 +0900

    おまけ

--- 初級java2.5/src/RectIf.java
+++ 初級java2.5/src/RectIf.java
@@ -1,15 +1,23 @@
 
 public class RectIf extends MyFrame{
 	public void run() {
-		int x=30;
-		setColor(0,128,0);
-		
-		for(int i=0;i<9;i++)
+		int x=300;
+		int y=30;
+		setColor(0,255,0);
+		/*for(int i=0;i<9;i++)//追加1
 		{
-			setColor(x,x,x);
-			fillRect(x,80,10,100);
-			x+=20;
-		}
+            fillRect(x,x,y,y);
+            x=x+y;
+			y=y+5;
+		}*/
+	
+	for(int i=0;i<9;i++)
+	{
+	       fillRect(x,30,20,y);
+	       fillRect(x,30,20,y); 
+	    x-=30;
+	    y+=20;
+	       System.out.println(x);
 	}
 
-}
+	}}
